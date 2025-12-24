1df4cc265f7b5bbe0efadc046bf015385e88bf27
Adam Cabrera <adamcabrera@mac.lan>
2025-12-23 19:43:50 -0800

    r017.51: Restore dirty hash fix and expand/collapse fix after rollback

--- client/your-extensions/widgets/query-simple/src/version.ts
+++ client/your-extensions/widgets/query-simple/src/version.ts
@@ -10,7 +10,7 @@
  */
 const BASE_VERSION = '1.19.0'
 const RELEASE_NUMBER = '017' // Increment on major changes (001, 002, 003, ...)
-const MINOR_VERSION = '50' // Increment on minor fixes between major releases (1, 2, 3, ...)
+const MINOR_VERSION = '51' // Increment on minor fixes between major releases (1, 2, 3, ...)
 
 export const WIDGET_VERSION = MINOR_VERSION && MINOR_VERSION !== '0'
   ? `${BASE_VERSION}-r${RELEASE_NUMBER}.${MINOR_VERSION}`
